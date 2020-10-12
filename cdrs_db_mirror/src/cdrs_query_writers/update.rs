e54d9785b6384cae1443a63f90d9c49b604841d2
Jasper Visser <jasper.visser@keylane.com>
2020-10-12 19:45:24 +0200

    Added clone derive on updateable columns

--- cdrs_db_mirror/src/cdrs_query_writers/update.rs
+++ cdrs_db_mirror/src/cdrs_query_writers/update.rs
@@ -83,6 +83,7 @@ pub(crate) fn generate(inf: &Inf, fn_name: &Ident, update: Update) -> TokenStrea
 
             let mut e = if inf.materialized_view.is_none() {
                 quote! {
+                    #[derive(Clone)]
                     pub enum #enum_ident {
                         #(#enum_cases(#tys)),*
                     }
