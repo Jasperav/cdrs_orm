d965e8092f48f17d43a27e84e6abcd569c9a84bc
Jasper Visser <jasper.visser@keylane.com>
2020-10-19 09:20:16 +0200

    Removed unused &self parameter

--- cdrs_db_mirror/src/cdrs_query_writers/truncate.rs
+++ cdrs_db_mirror/src/cdrs_query_writers/truncate.rs
@@ -11,7 +11,7 @@ pub fn generate(inf: &Inf, fn_name: &Ident) -> TokenStream {
         impl #name {
             pub const TRUNCATE_QUERY: &'static str = concat!("truncate ", stringify!(#name));
 
-            pub fn #fn_name(&self) -> (&'static str, cdrs::query::QueryValues) {
+            pub fn #fn_name() -> (&'static str, cdrs::query::QueryValues) {
                 (#name::TRUNCATE_QUERY, cdrs::query::QueryValues::SimpleValues(vec![]))
             }
         }
