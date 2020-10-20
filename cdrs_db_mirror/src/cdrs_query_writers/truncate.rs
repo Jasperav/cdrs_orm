3d0c0632178007ddc0d37e9aa83e93f06766a542
Jasper Visser <jasper.visser@keylane.com>
2020-10-20 20:25:07 +0200

    truncate query now truncates the table name and not necessary the struct name

--- cdrs_db_mirror/src/cdrs_query_writers/truncate.rs
+++ cdrs_db_mirror/src/cdrs_query_writers/truncate.rs
@@ -6,10 +6,12 @@ use syn::Ident;
 /// Generates a truncate query
 pub fn generate(inf: &Inf, fn_name: &Ident) -> TokenStream {
     let name = inf.name;
+    let table_name = inf.table_name;
+    let query = format!("truncate {}", table_name);
 
     quote! {
         impl #name {
-            pub const TRUNCATE_QUERY: &'static str = concat!("truncate ", stringify!(#name));
+            pub const TRUNCATE_QUERY: &'static str = #query;
 
             pub fn #fn_name() -> (&'static str, cdrs::query::QueryValues) {
                 (#name::TRUNCATE_QUERY, cdrs::query::QueryValues::SimpleValues(vec![]))
