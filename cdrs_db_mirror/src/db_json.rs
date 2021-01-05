9179f86a347e637c60749fc749758688f7dfb66a
Jasper Visser <jasper.visser@keylane.com>
2021-01-05 11:11:50 +0100

    Inlining clippy warning

--- cdrs_db_mirror/src/db_json.rs
+++ cdrs_db_mirror/src/db_json.rs
@@ -6,6 +6,8 @@ pub(crate) fn db_json(derive_input: DeriveInput) -> TokenStream {
     let name = derive_input.ident;
 
     quote! {
+        // Not sure how to make this works with From
+        #[allow(clippy::from_over_into)]
         impl Into<cdrs::types::value::Bytes> for #name {
             fn into(self) -> cdrs::types::value::Bytes {
                 serde_json::to_string(&self).unwrap().into()
