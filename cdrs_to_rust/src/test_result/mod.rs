8ffea1fc7958a23771aa3006fabacc52b9659dee
Jasper Visser <jasper.visser@keylane.com>
2021-04-13 19:25:25 +0200

    Fixed typo

--- cdrs_to_rust/src/test_result/mod.rs
+++ cdrs_to_rust/src/test_result/mod.rs
@@ -1,9 +1,5 @@
-#![allow(unknown_lints)]
-#![allow(clippy::all)]
-#![rustfmt::skip]
-#![allow(unused_attributes)]
 // @generated, do not edit
 pub mod test2;
+pub mod test2materialized;
 pub mod test_table;
 pub mod testjsonmapping;
-pub mod test2materialized;
